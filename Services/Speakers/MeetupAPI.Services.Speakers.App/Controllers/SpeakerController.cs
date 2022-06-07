4f73765c0133f18d4a33531e46af2d6c7ef815dd
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-06-07 04:02:43 +0300

    Add MeetupAPI.Services.Speakers.App, Add Meetup.Shared.Services

---
+++ Services/Speakers/MeetupAPI.Services.Speakers.App/Controllers/SpeakerController.cs
@@ -0,0 +1,49 @@
+﻿using MeetupAPI.Services.Speakers.Contract;
+using MeetupAPI.Services.Speakers.Contract.Model;
+using MeetupAPI.Services.Speakers.Contract.Model.Command;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetupAPI.Services.Speakers.App.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SpeakerController : Controller
+{
+    private readonly ISpeakerService _speakerService;
+
+    public SpeakerController(
+        ISpeakerService speakerService)
+    {
+        _speakerService = speakerService;
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Speaker>> Get(
+    [FromRoute] string id,
+    CancellationToken cancellationToken = default)
+    {
+        var result = await _speakerService
+            .Get(id, cancellationToken)
+            .WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Speaker>> Create(
+      [FromBody] CreateSpeakerCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        var result = await _speakerService
+            .Create(command, cancellationToken)
+            .WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+}
