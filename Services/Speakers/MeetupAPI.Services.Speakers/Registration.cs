a074d04dfead57a14ab036fde0da7bb1adeccb62
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-06-07 03:31:28 +0300

    Add Registration.cs to MeetupAPI.Services.Speakers

---
+++ Services/Speakers/MeetupAPI.Services.Speakers/Registration.cs
@@ -0,0 +1,25 @@
+﻿using MeetupAPI.Services.Speakers.Context;
+using MeetupAPI.Services.Speakers.Contract;
+using MeetupAPI.Services.Speakers.Services;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetupAPI.Services.Speakers;
+
+public static class Registration
+{
+    public static IServiceCollection AddSpeakers(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContextPool<SpeakersDbContext>(
+            (s, b) =>
+                b.UseNpgsql(configuration.GetConnectionString("SpeakersDb")));
+
+        services.AddScoped<ISpeakerService, SpeakerService>();
+
+        return services;
+    }
+}
