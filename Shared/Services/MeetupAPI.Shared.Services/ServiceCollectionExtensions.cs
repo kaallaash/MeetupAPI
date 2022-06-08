ea143e438e65177eb6cb931551ceebe27fc64c3f
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-06-08 17:39:02 +0300

    Add ServiceCollectionExtensions.sc to MeetupAPI.Shared.Services

---
+++ Shared/Services/MeetupAPI.Shared.Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeetupAPI.Shared.Services;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddServiceClient<TServiceContract, TImplementation>(
+        this IServiceCollection services,
+        string serviceUriName)
+        where TServiceContract : class
+        where TImplementation : class, TServiceContract
+
+    {
+        services.AddHttpClient<TServiceContract, TImplementation>(
+            (sp, client) =>
+            {
+                var configuration = sp.GetRequiredService<IConfiguration>();
+                var baseAddress = configuration.GetServiceUri(serviceUriName);
+                client.BaseAddress = baseAddress;
+            });
+    }
+}
