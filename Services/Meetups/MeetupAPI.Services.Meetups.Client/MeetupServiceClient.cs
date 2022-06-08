173da3aa9f96ae3b2acac4cfa19669f7e7f09a42
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-06-08 17:12:26 +0300

    Add MeetupAPI.Services.Meetups.Client project and refactoring

---
+++ Services/Meetups/MeetupAPI.Services.Meetups.Client/MeetupServiceClient.cs
@@ -0,0 +1,38 @@
+﻿using MeetupAPI.Services.Meetups.Contract;
+
+namespace MeetupAPI.Services.Meetups.Client;
+
+public partial class MeetupServiceClient : IMeetupService
+{
+    public async Task<Meetup> Get(
+        string id,
+        CancellationToken cancellationToken = default)
+    {
+        return await GetAsync(id, cancellationToken)
+            .ConfigureAwait(false);
+    }
+
+    public async Task<Meetup> Create(
+        CreateMeetupCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        return await CreateAsync(command, cancellationToken)
+            .ConfigureAwait(false);
+    }
+
+    public async Task<Meetup> Update(
+        UpdateMeetupCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        return await UpdateAsync(command, cancellationToken)
+            .ConfigureAwait(false);
+    }
+
+    public async Task Delete(
+        string id,
+        CancellationToken cancellationToken = default)
+    {
+        await DeleteAsync(id, cancellationToken)
+            .ConfigureAwait(false);
+    }
+}
---
+++ Services/Meetups/MeetupAPI.Services.Meetups.Client/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using MeetupAPI.Services.Meetups.Contract.Model.Commands;
+global using MeetupAPI.Services.Meetups.Contract.Model;
+global using MeetupAPI.Shared.Services.Api;
